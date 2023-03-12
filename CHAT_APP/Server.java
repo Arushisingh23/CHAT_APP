f87ecd181956e73091d69914d6346b4a174ceac4
Arushi singh <85683967+Arushisingh23@users.noreply.github.com>
2023-03-12 19:59:02 +0530

    Add files via upload

---
+++ CHAT_APP/Server.java
@@ -0,0 +1,18 @@
+import java.net.*;
+
+class Server {
+
+    
+    
+    public Server()  //constructor
+    {
+
+    } 
+    
+    public static void main(String[] args) {
+        System.out.println("this is server ");
+        new Server();  //object creation i.e calling
+
+
+    }
+}
